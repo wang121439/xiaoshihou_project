73a8e56a1cd8d46dc3c2e76597553e051c0dde68
wang <1135889390@qq.com>
2021-04-22 08:04:36 +0800

    第一次提交xiaoshihou

---
+++ pages/product/product.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		<text>商品</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="less">
+
+</style>
---
+++ pages/service/service.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		<text>客服</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="less">
+
+</style>
